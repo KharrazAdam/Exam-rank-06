84fdd5b08f4910dd9be21c94cd06ca7bbbcd826f
KharrazAdam <adam.kharraz13@gmail.com>
2023-10-25 02:26:18 +0100

    fix id in sebding messages

--- mini_serv.c
+++ mini_serv.c
@@ -18,37 +18,49 @@ void    ft_pustr(char *str)
     }
 }
 
-int start(int serSocket, struct sockaddr_in *address)
+void    sendAll(int stop, const char *buf, int id, int *clients)
+{
+    for (int i = 0; i <= stop; i++)
+    {
+        if (clients[i] != id)
+           send(i, buf, strlen(buf), 0);
+    } 
+}
+
+struct client
+{
+    int fd;
+    int id;
+};
+
+int start(int serSocket)
 {
     int nfds = serSocket;
-
     int clientFds[1024] = {0};
-    clientFds[serSocket] = serSocket;
+    
     fd_set readfds;
     FD_ZERO(&readfds);
     FD_SET(serSocket, &readfds);
 
+    char    msg[1024];
+    int     id = 0;
     while (1)
     { 
         fd_set tmp;
         FD_ZERO(&tmp);
         tmp = readfds;
-        printf("Waiting %d\n", serSocket);
         int retValue = select(nfds + 1, &tmp, NULL, NULL, NULL);
-        printf("Processing\n");
         if (retValue == -1)
         {
             write(2, "Fatal error\n", 13);
             exit(1);
         }
-        for (size_t i = 0; i < nfds + 1; i++)
+        for (int i = 0; i < nfds + 1; i++)
         {
             if (FD_ISSET(i, &tmp))
             {
-                // socket server send : accept new connection
                 if (i == serSocket)
                 {
-                    printf("connect\n");
                     int connect = accept(serSocket, NULL, NULL);
                     if (connect == -1)
                     {
@@ -56,29 +68,30 @@ int start(int serSocket, struct sockaddr_in *address)
                         exit(1);
                     }
                     nfds = nfds > connect ? nfds : connect; 
-                    clientFds[connect] = connect;
+                    clientFds[connect] = id;
                     FD_SET(connect, &readfds);
-                    // send message to all usrs rah tconnecta;
+                    bzero(msg, sizeof(msg));
+                    sprintf(msg, "server: client %d just arrived\n", id);
+                    sendAll(nfds, msg, id, clientFds);
+                    id++;
                 }
                 else 
                 {
                     char buf[1024]; 
                     bzero(buf, sizeof(buf));
                     int rec = recv(i, buf, sizeof(buf), 0);
-                    // client disconected;
                     if (rec == 0)
                     {
-                        // clientFds[i] = -2;
-                        ft_pustr("client disconnected\n");
+                        bzero(buf, sizeof(buf));
+                        sprintf(buf, "%d disconnected\n", clientFds[i]);
+                        ft_pustr(buf);
                         FD_CLR(i, &readfds);
                         close(i);
                     }
                     else
                     {
                         send(i, buf, strlen(buf), 0);
-                        // ft_pustr(buf);
                     }
-                   
                 }
             }
         }
@@ -113,7 +126,7 @@ int    createServer(int port)
         write(2, "Fatal error\n", 13);
         exit(1);
     }
-    if (start(sock, &address) == -1)
+    if (start(sock) == -1)
         return 1;
 
     return sock;
